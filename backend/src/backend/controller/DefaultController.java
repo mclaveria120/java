5b12de12280f913bfc1822adee701d334dcb4dce
Mariano Garcia Claveria <mclaveria@mclaveria.local>
2017-01-23 12:13:15 -0500

    spring,hibernate,resteasy initial setup

---
+++ backend/src/backend/controller/DefaultController.java
@@ -0,0 +1,24 @@
+package backend.controller;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/default")
+public class DefaultController {
+
+	
+	@GET
+	@Path("/test")
+	public Response test() {
+		return Response.ok().entity("Ok").build();
+	}
+}
